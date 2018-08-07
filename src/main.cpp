8389ea0b002f8837c79e74317bfd8ed02180baff
Karab <karab.sze@gmail.com>
2018-08-08 00:16:12 +0800

    The car even out of the track just after 3 second. Response too slow

--- src/main.cpp
+++ src/main.cpp
@@ -35,8 +35,10 @@ int main()
   PID pid;
   // First Trial. Oscillate too much
   // pid.Init(0.2, 0.004, 3.000);
-  // Second Trial
-  pid.Init(0.1, 0.004, 3.000);
+  // Second Trial. Even fast to out of track. Response too slow
+  // pid.Init(0.1, 0.004, 3.000);
+  // Third Trial
+  // pid.Init(0.3, 0.004, 3.000);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
