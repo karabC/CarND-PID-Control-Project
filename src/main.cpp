c495082cec8690df6a6b393a8797ba06feb3a124
Karab <karab.sze@gmail.com>
2018-08-08 00:18:20 +0800

    remove comment (bug fix)

--- src/main.cpp
+++ src/main.cpp
@@ -38,7 +38,7 @@ int main()
   // Second Trial. Even fast to out of track. Response too slow
   // pid.Init(0.1, 0.004, 3.000);
   // Third Trial
-  // pid.Init(0.3, 0.004, 3.000);
+  pid.Init(0.3, 0.004, 3.000);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
